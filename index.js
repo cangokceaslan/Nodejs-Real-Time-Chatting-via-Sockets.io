938c9f40ab894979b4736e08a49a4137aa441e5a
CAN GÖKÇEASLAN <mrthack@hotmail.com>
2019-07-25 04:03:58 +0300

    v1
    v1

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var io = require("socket.io")(http);
 var path = __dirname+'/';
 var md5 = "";
 var css = __dirname+'/css/styles.css';
-app.use(express.static(__dirname + '/public'));
 app.get(path, function(req, res){
   if(path==null){
-    path = '/index.html';
+    path = __dirname+'/index.html';
   }
   res.sendFile(__dirname + path);
 });
