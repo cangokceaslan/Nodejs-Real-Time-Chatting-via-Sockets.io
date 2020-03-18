10f52039cf89f8e73ea019d203095dda729ba61d
CAN GÖKÇEASLAN <mrthack@hotmail.com>
2020-03-19 01:40:09 +0300

    v1.0
    v1.0

---
+++ chatting/index.js
@@ -0,0 +1,30 @@
+var app = require("express")();
+var io = require("socket.io")(801);
+var path = "";
+var md5 = "";
+
+app.get("/css/styles.css",function(req,res){
+    res.sendFile(__dirname+"/css/styles.css");
+})
+app.get("",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+app.get("/notification.mp3",function(req,res){
+    res.sendFile(__dirname+"/notification.mp3");
+})
+app.get("/message.html",function(req,res){
+    res.sendFile(__dirname+"/message.html");
+    md5 = req.query.md5;
+})
+app.listen((process.env.PORT || 80),function(){
+    console.log("80. Port Dinleniliyor");
+})
+io.on("connection",function(socket){
+    socket.on("disconnect",function(){
+        console.log("Bir kullanıcı odadan ayrıldı");
+    })
+    socket.on(md5,function(msg){
+        io.emit(md5,msg);
+        console.log(msg);
+    })
+})
